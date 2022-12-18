be3fb406398fa663e3450edfb631964d28bd82ea
mochsner <marcaochsner@gmail.com>
2022-12-17 23:47:52 -0500

    Add automapper to Retrieve Recipes

--- angular-dotnet/Program.cs
+++ angular-dotnet/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 await using var ctx = new GrocerContext();
