f831a690fe25cf8e218e746db6ee03e7136bca25
mochsner <marcaochsner@gmail.com>
2022-12-17 23:25:39 -0500

    Basic automapper

---
+++ angular-dotnet/Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using angular_dotnet.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace angular_dotnet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RecipeController : ControllerBase
+{
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public RecipeController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    // [HttpGet]
+    // public IEnumerable<RecipeDto> GetRecipes()
+    // {
+    //     
+    // }
+}
--- angular-dotnet/Program.cs
+++ angular-dotnet/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 await using var ctx = new GrocerContext();
 
