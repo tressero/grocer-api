354157edd86efba8d70eaa92d6c7084ca2ffb7e8
mochsner <marcaochsner@gmail.com>
2022-12-17 23:47:33 -0500

    Delete accidental folder

--- angular-dotnet/Controllers/RecipeController.cs
+++ angular-dotnet/Controllers/RecipeController.cs
@@ -1,4 +1,7 @@
-﻿using angular_dotnet.Dto;
+﻿using angular_dotnet.DbContext;
+using angular_dotnet.Dto;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace angular_dotnet.Controllers;
@@ -8,16 +11,23 @@ namespace angular_dotnet.Controllers;
 public class RecipeController : ControllerBase
 {
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<RecipeController> _logger;
+    private readonly IMapper _mapper;
 
-    public RecipeController(ILogger<WeatherForecastController> logger)
+    public RecipeController(ILogger<RecipeController> logger, IMapper mapper)
     {
         _logger = logger;
+        _mapper = mapper;
     }
 
-    // [HttpGet]
-    // public IEnumerable<RecipeDto> GetRecipes()
-    // {
-    //     
-    // }
+    [HttpGet]
+    public IEnumerable<RecipeDto> GetRecipes()
+    {
+        using (GrocerContext context = new GrocerContext())// TODO: Don't call context directly from controller, use unitOfWork ?
+        {
+            var recipes = (from r in context.Recipe select r);
+            var recipeDtos = recipes.ProjectTo<RecipeDto>(_mapper.ConfigurationProvider);
+            return recipeDtos;
+        }
+    }
 }
