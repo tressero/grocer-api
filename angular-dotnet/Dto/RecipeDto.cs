e9523ed257efb3bd5c99668d917897deee16017a
mochsner <marcaochsner@gmail.com>
2022-12-17 23:07:02 -0500

    Add Dtos

---
+++ angular-dotnet/Dto/RecipeDto.cs
@@ -0,0 +1,10 @@
+namespace angular_dotnet.Dto;
+
+public class RecipeDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    
+    public List<RecipeIngredientDto> RecipeIngredients { get; set; }
+}
