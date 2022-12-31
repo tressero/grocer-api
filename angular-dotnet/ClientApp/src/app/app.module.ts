ccff5f6afc3312ddc11c9d7ee1cb6ed5f98b9906
mochsner <marcaochsner@gmail.com>
2022-12-30 22:37:53 -0500

    Tryig to get an angular autocomplete as component working ?

--- angular-dotnet/ClientApp/src/app/app.module.ts
+++ angular-dotnet/ClientApp/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /* UI components */
 import { MatTableModule } from "@angular/material/table";
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -13,25 +15,36 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { ExpansionOverviewExample } from "./components/expansion-overview-example";
+import {AutocompleteExample} from "./components/autocomplete-example";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatInputModule} from "@angular/material/input";
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ExpansionOverviewExample,
+    AutocompleteExample
   ],
-    imports: [
-        BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
-        HttpClientModule,
-        FormsModule,
-        RouterModule.forRoot([
-            {path: '', component: HomeComponent, pathMatch: 'full'},
-            {path: 'counter', component: CounterComponent},
-            {path: 'fetch-data', component: FetchDataComponent},
-        ]),
-        MatTableModule,
-    ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'counter', component: CounterComponent},
+      {path: 'fetch-data', component: FetchDataComponent},
+    ]),
+    MatTableModule,
+    MatExpansionModule,
+    MatAutocompleteModule,
+    MatInputModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- angular-dotnet/ClientApp/src/app/home/home.component.ts
+++ angular-dotnet/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Inject, ViewChild} from '@angular/core';
+import {MatAccordion} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['home.component.css'],
 })
 export class HomeComponent {
+  panelOpenState = false;
 }
