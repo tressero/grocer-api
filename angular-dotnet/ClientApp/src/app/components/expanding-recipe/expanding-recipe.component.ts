857cea1cc77147d53c90cc6a3d12192bc0d0292d
mochsner <marcaochsner@gmail.com>
2023-01-04 21:59:10 -0500

    Compute displayedRecipeIngredients for reading

--- angular-dotnet/ClientApp/src/app/components/expanding-recipe/expanding-recipe.component.ts
+++ angular-dotnet/ClientApp/src/app/components/expanding-recipe/expanding-recipe.component.ts
@@ -3,6 +3,7 @@ import {Recipe} from "../../models/recipe";
 import {Ingredient} from "../../models/ingredient";
 import {RecipeIngredient} from "../../models/recipe-ingredient";
 import {StoreSection} from "../../models/store-section";
+import {DisplayedRecipeIngredient} from "../../models/displayed-recipe-ingredient";
 
 /**
  * @title Basic expansion panel
@@ -15,8 +16,37 @@ import {StoreSection} from "../../models/store-section";
 export class ExpandingRecipe {
   panelOpenState = false;
   @Input() recipe!: Recipe;
-  @Input() recipeIngredients!: RecipeIngredient[];
-  @Input() Ingredients!: Ingredient[];
+  @Input()
+  get recipeIngredients(): RecipeIngredient[] { return this._recipeIngredients; }
+  set recipeIngredients(recipeIngredients: RecipeIngredient[]) {
+    let tmp = recipeIngredients.filter(ri => ri.recipeId === this.recipe.id);
+    console.log('tmp',tmp);
+    for (let ri of tmp) {
+      let ingredient = this.ingredients.find(i => i.id === ri.ingredientId);
+      console.log('ingredient',ingredient);
+      // @ts-ignore
+      let newRi: DisplayedRecipeIngredient = {
+        recipeId: this.recipe.id,
+        ingredientCount: ri.ingredientCount,
+        ingredientId: ri.ingredientId,
+        ingredientName: ingredient?.name ?? "",
+        ingredientUnit: ingredient?.unit ?? "",
+        ingredientDescription: ingredient?.description ?? ""
+      };
+      this.displayedRecipeIngredients.push(newRi);
+    }
+    console.log('displayedRecipeIngredients',this.displayedRecipeIngredients);
+  }
+  private _recipeIngredients!: RecipeIngredient[];
+
+  @Input() ingredients!: Ingredient[];
+
+  displayedRecipeIngredients: DisplayedRecipeIngredient[] = [];
+
+  ngOnInit() {
+    /* can't set displayedREcipeIngredients here because ti doesn't seem to capture them*/
+  }
+
 }
 
 
