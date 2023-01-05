767496bf99e4f1e5d79b08cd601fd78bafcd3c54
mochsner <marcaochsner@gmail.com>
2023-01-04 21:59:10 -0500

    Compute displayedRecipeIngredients for reading

---
+++ angular-dotnet/ClientApp/src/app/models/displayed-recipe-ingredient.ts
@@ -0,0 +1,63 @@
+
+export interface DisplayedRecipeIngredient {
+  ingredientCount: string;
+
+  recipeId: string;
+
+  ingredientId: string;
+  ingredientName: string;
+  ingredientDescription: string;
+  ingredientUnit: string;
+
+  // storeLocationId: string;
+  // storeLocationName: string;
+}
+
+
+// export const RecipeIngredientColumns = [
+//   {
+//     key: 'isSelected',
+//     type: 'isSelected',
+//     label: '',
+//   },
+//   {
+//     key: 'ingredientCount',
+//     type: 'number',
+//     label: 'Ingredient Count',
+//     required: true,
+//   },
+//   {
+//     key: 'ingredientName',
+//     type: 'text',
+//     label: 'Ingredient Name',
+//     required: true,
+//   },
+//   {
+//     key: 'ingredientUnit',
+//     type: 'text',
+//     label: 'Unit',
+//     required: true,
+//   },
+//   {
+//     key: 'ingredientDescription',
+//     type: 'text',
+//     label: 'Ingredient Description',
+//   },
+//   {
+//     key: 'unit',
+//     type: 'email',
+//     label: 'Unit / Measurement Type',
+//     required: true,
+//   },
+//   {
+//     key: 'storeSectionId',
+//     type: 'selector',
+//     label: 'StoreSectionId',
+//     required: true,
+//   },
+//   {
+//     key: 'isEdit',
+//     type: 'isEdit',
+//     label: '',
+//   },
+// ];
