6986788b8f9d9b683d0570e97da93b293431c77c
mochsner <marcaochsner@gmail.com>
2023-01-12 22:21:00 -0500

    TODO: addOrUpdate recipeIngredient + create new recipe / remove recipe

---
+++ angular-dotnet/ClientApp/src/app/services/recipe.service.ts
@@ -0,0 +1,36 @@
+import {Inject, Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import {Ingredient} from '../models/ingredient';
+import {Recipe} from "../models/recipe";
+import {Recipe} from "../models/recipe-ingredient";
+import {StoreSection} from "../models/store-section";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  private serviceUrl = '';
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') serviceUrl: string) {
+    this.serviceUrl = serviceUrl + "Recipe";
+  }
+
+  getRecipes(): Observable<Recipe[]> {
+    debugger;
+    return this.http
+      .get(`${this.serviceUrl}/GetAll`)
+      .pipe<Recipe[]>(map((data: any) => data));
+  }
+
+  addOrUpdate(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(`${this.serviceUrl}/addOrUpdate`, recipe);
+  }
+
+  delete(recipe: Recipe): Observable<Ingredient> {
+    return this.http.delete<Ingredient>(`${this.serviceUrl}/delete/${recipe.id}/`);
+  }
+
+}
