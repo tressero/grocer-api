6899f57debb07188a30781522f8832b8272d4a1a
Marc Ochsner <marc.ochsner@woundzoom.com>
2023-01-29 14:47:53 -0500

    Add grocery list SQL

--- angular-dotnet/Migrations/SQL/Helpers/Get-Grocery-List.sql
+++ angular-dotnet/Migrations/SQL/Helpers/Get-Grocery-List.sql
@@ -1,8 +1,9 @@
 select R."Name", SS."Name", I."Name", RI."IngredientCount", I."Unit"  from "Recipe" R
-left join "RecipeIngredientJunction" RI
-    on RI."RecipeId" = R."Id"
-left join "Ingredient" I 
-    on I."Id" = RI."IngredientId"
-left join "StoreSection" SS 
-    on I."StoreSectionId" = SS."Id"
-order by SS."Name"
+                                                                               left join "RecipeIngredientJunction" RI
+                                                                                         on RI."RecipeId" = R."Id"
+                                                                               left join "Ingredient" I
+                                                                                         on I."Id" = RI."IngredientId"
+                                                                               left join "StoreSection" SS
+                                                                                         on I."StoreSectionId" = SS."Id"
+where R."Name" NOT LIKE 'Vegetable Soup'
+order by SS."Name", I."Name"
