2e81f5f2a3daedd9c10e55ef49b308dc583e9421
Marc Ochsner <marc.ochsner@woundzoom.com>
2023-02-05 20:08:55 -0500

    Updated groceries 2023-02-05

--- angular-dotnet/Migrations/SQL/Helpers/Get-Grocery-List.sql
+++ angular-dotnet/Migrations/SQL/Helpers/Get-Grocery-List.sql
@@ -5,5 +5,14 @@ select R."Name", SS."Name", I."Name", RI."IngredientCount", I."Unit"  from "Reci
                                                                                          on I."Id" = RI."IngredientId"
                                                                                left join "StoreSection" SS
                                                                                          on I."StoreSectionId" = SS."Id"
-where R."Name" NOT LIKE 'Vegetable Soup'
+where 
+--   or R."Name" LIKE 'Vegetable Soup'
+--   or 
+    R."Name" LIKE 'Veggie Bake'
+--   or R."Name" LIKE 'Instant Pot Chicken Cacciatore'
+  or R."Name" LIKE 'Portobello Fajitas'
+--   or R."Name" LIKE 'Quick Vegetarian Chili'
+   or R."Name" LIKE 'Chickpeas, Sweet Pot + Broc'
+   or R."Name" LIKE 'Weekly / Bi-weekly Purchases'
+   or R."Name" LIKE 'White Chicken Chili'
 order by SS."Name", I."Name"
