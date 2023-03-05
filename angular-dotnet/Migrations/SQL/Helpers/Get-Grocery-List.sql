a7f9c826a4b459e08425cf6ad7748214362ca566
Marc Ochsner <marc.ochsner@woundzoom.com>
2023-03-05 15:10:41 -0500

    update list with note on pgres

--- angular-dotnet/Migrations/SQL/Helpers/Get-Grocery-List.sql
+++ angular-dotnet/Migrations/SQL/Helpers/Get-Grocery-List.sql
@@ -5,15 +5,24 @@ select R."Name", SS."Name", I."Name", RI."IngredientCount", I."Unit"  from "Reci
                                                                                          on I."Id" = RI."IngredientId"
                                                                                left join "StoreSection" SS
                                                                                          on I."StoreSectionId" = SS."Id"
-where 
+where
+   R."Name" LIKE 'Weekly / Bi-weekly Purchases'
+
 --   or R."Name" LIKE 'Vegetable Soup'
 --   or 
-    R."Name" LIKE 'Veggie Bake'
---   or R."Name" LIKE 'Instant Pot Chicken Cacciatore'
+    or R."Name" LIKE 'Veggie Bake'
+  or R."Name" LIKE 'Instant Pot Chicken Cacciatore'
   or R."Name" LIKE 'Portobello Fajitas'
 --   or R."Name" LIKE 'Quick Vegetarian Chili'
-   or R."Name" LIKE 'Chickpeas, Sweet Pot + Broc'
-   or R."Name" LIKE 'Weekly / Bi-weekly Purchases'
-   or R."Name" LIKE 'White Chicken Chili'
+--    or R."Name" LIKE 'Chickpeas, Sweet Pot + Broc'
+--    or R."Name" LIKE 'White Chicken Chili'
    or R."Name" LIKE 'WW Salmon'
+--    or R."Name" like 'WW Beef + broc%'
+   or R."Name" like 'Grilled Ribeye + Veggies'
+   or R."Name" like 'WW Beef & Bean%'
 order by SS."Name", I."Name"
+-- order by R."Name"
+
+/* NOTE: POSTGRES LIKE IS CASE SENSITIVE */
+
+select * from "Recipe"
