a9e2688463951571c98341882352c2b75f940935
mochsner <marcaochsner@gmail.com>
2023-03-26 17:20:32 -0400

    add blank ox-recipe-selector component

---
+++ angular-dotnet/ClientApp/src/app/components/ox-recipe-selector/ox-recipe-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ox-recipe-selector',
+  templateUrl: './ox-recipe-selector.component.html',
+  styleUrls: ['./ox-recipe-selector.component.css']
+})
+export class OxRecipeSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
